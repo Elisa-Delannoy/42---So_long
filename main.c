8c6122656d4ec433804f940e44f86e8fef27d10c
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-01-31 15:45:02 +0100

    to begin

--- main.c
+++ main.c
@@ -11,27 +11,55 @@
 /* ************************************************************************** */
 
 #include "mlx_linux/mlx.h"
+#include <stddef.h> 
 
 typedef struct	s_data {
 	void	*img;
+	void	*img2;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
+	int     width;
+    int     height;
 	int		endian;
 }				t_data;
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("Hello from key_hook!\n");
+	return (0);
+}
 
 int	main()
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
 
-	mlx = mlx_init(); /*obligatoire pour lancer mlx*/
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!"); /*hauteur,longueur, titre*/
-	mlx_loop(mlx); /*lancer la fenetre*/
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+	t_vars	vars;
+	t_data	img;
+	t_data	img2;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 900, 700, "Hello world!");
+	img.img = mlx_xpm_file_to_image(vars.mlx, "/home/edelanno/42-So_long/images/santa_R.xpm", &img.width, &img.height);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	img2.img2 = mlx_xpm_file_to_image(vars.mlx, "/home/edelanno/42-So_long/images/wall1.xpm",&img.width, &img.height);
+	mlx_put_image_to_window(vars.mlx, vars.win, img2.img2,80,0);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
+
+
+
 }
 
